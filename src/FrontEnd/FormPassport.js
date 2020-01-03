f63d412888d294a67ce9a8762a2c2cb5fc2b9472
hungtp <hungtp.personal@gmail.com>
2020-01-03 21:08:50 +0700

    complete add value

--- src/FrontEnd/FormPassport.js
+++ src/FrontEnd/FormPassport.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Tooltip, Icon, Select, Row, Col, Button, DatePicker } from 'antd';
+import { Form, Input, Tooltip, Icon, Select, Row, Col, Button, DatePicker, Modal } from 'antd';
 import { provinces } from './../Data/provinces';
 import { district } from './../Data/districts';
 import { nations } from './../Data/nations';
@@ -18,7 +18,8 @@ class RegistrationForm extends React.Component {
       autoCompleteResult: [],
       AddressProvince: null,
       StayingProvince: null,
-      ApointmentProvince: null
+      ApointmentProvince: null,
+      modalVisibility: false,
     };
   }
 
@@ -30,10 +31,12 @@ class RegistrationForm extends React.Component {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        axios.post("http://localhost:3500/passport/post",{
+        axios.post("http://localhost:3500/passport/post", {
           params: values
         }).then(res => {
-          console.log(res);
+          this.setState({ modalVisibility: true });
+          const { form } = this.props;
+          form.resetFields();
         });
       }
     });
@@ -87,214 +90,221 @@ class RegistrationForm extends React.Component {
     };
 
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Row>
-          <Col span={8}>
-            <Form.Item label="Fullname">
-              {getFieldDecorator('fullname', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input your Fullname!',
-                  },
-                ],
-              })(<Input />)}
-            </Form.Item>
-          </Col>
-          <Col span={7} offset={1}>
-            <Form.Item label="Birthday">
-              {getFieldDecorator('birthday', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input your Birthday!',
-                  },
-                ],
-              })(<DatePicker />)}
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item label="Birthplace">
-              {getFieldDecorator('birthplace', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your Birthplace!',
-                  },
-                ],
-              })(<Select>
-                {provinces.map(p => <Option value={p.name} key={p.value}>{p.name}</Option>)}
-              </Select>)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={6}>
-            <Form.Item label="Gender">
-              {getFieldDecorator('gender', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your Gender!',
-                  }
-                ],
-              })(<Select>
-                <Option value="1">Male</Option>
-                <Option value="0">Female</Option>
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col span={5} offset={1}>
-            <Form.Item label="Nation">
-              {getFieldDecorator('nation', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your nation'
-                  }
-                ]
-              })(<Select>
-                {nations.map(n => <Option value={n.name} key={n.value}>{n.name}</Option>)}
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col span={5} offset={1}>
-            <Form.Item label="Religion">
-              {getFieldDecorator('religion', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your religion'
-                  }
-                ]
-              })(<Select>
-                {religions.map(r => <Option value={r.name} key={r.value}>{r.name}</Option>)}
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col span={5} offset={1}>
-            <Form.Item label="Phone Number">
-              {getFieldDecorator('phonenumber', {
-                rules: [{
-                  required: true
-                  , message: 'Please input your Phone Number!'
-                }],
-              })(<Input />)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <Form.Item label={<span>Address Province&nbsp;
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Row>
+            <Col span={8}>
+              <Form.Item label="Fullname">
+                {getFieldDecorator('fullname', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input your Fullname!',
+                    },
+                  ],
+                })(<Input />)}
+              </Form.Item>
+            </Col>
+            <Col span={7} offset={1}>
+              <Form.Item label="Birthday">
+                {getFieldDecorator('birthday', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input your Birthday!',
+                    },
+                  ],
+                })(<DatePicker />)}
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item label="Birthplace">
+                {getFieldDecorator('birthplace', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select your Birthplace!',
+                    },
+                  ],
+                })(<Select>
+                  {provinces.map(p => <Option value={p.name} key={p.value}>{p.name}</Option>)}
+                </Select>)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={6}>
+              <Form.Item label="Gender">
+                {getFieldDecorator('gender', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select your Gender!',
+                    }
+                  ],
+                })(<Select>
+                  <Option value="1">Male</Option>
+                  <Option value="0">Female</Option>
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col span={5} offset={1}>
+              <Form.Item label="Nation">
+                {getFieldDecorator('nation', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select your nation'
+                    }
+                  ]
+                })(<Select>
+                  {nations.map(n => <Option value={n.name} key={n.value}>{n.name}</Option>)}
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col span={5} offset={1}>
+              <Form.Item label="Religion">
+                {getFieldDecorator('religion', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select your religion'
+                    }
+                  ]
+                })(<Select>
+                  {religions.map(r => <Option value={r.name} key={r.value}>{r.name}</Option>)}
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col span={5} offset={1}>
+              <Form.Item label="Phone Number">
+                {getFieldDecorator('phonenumber', {
+                  rules: [{
+                    required: true
+                    , message: 'Please input your Phone Number!'
+                  }],
+                })(<Input />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={8}>
+              <Form.Item label={<span>Address Province&nbsp;
               <Tooltip title="write based on your Registration book">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>}>
-              {getFieldDecorator('addprovince', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your Address Province!'
-                  }
-                ]
-              })(<Select onChange={value => this.setAddressProvince(value)}>
-                {provinces.map(p => <Option key={p.value} value={p.value}>{p.name}</Option>)}
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col span={7} offset={1}>
-            <Form.Item label={<span>Address District&nbsp;
-              <Tooltip title="write based on Registration book">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>}>
-              {getFieldDecorator('adddistrict', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select your Address District!',
-                  }
-                ],
-              })(<Select>
-                {this.getDistrictByProvince(this.state.AddressProvince).map(d => <Option
-                  key={d.value}
-                  value={d.name}>
-                  {d.name}
-                </Option>)}
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col span={7} offset={1}>
-            <Form.Item label={<span>Address Detail&nbsp;
-              <Tooltip title="write based on Registration book">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>}>
-              {getFieldDecorator('adddetail', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input your Address Detail'
-                  }
-                ],
-              })(<Input />)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={12}>
-            <Form.Item label={
-              <span>Father Name&nbsp;
-              <Tooltip title="If you have father, you must input this field">
                   <Icon type="question-circle-o" />
-                </Tooltip></span>
-            }>
-              {getFieldDecorator('fatherfullname')(<Input />)}
-            </Form.Item>
-          </Col>
-          <Col span={11} offset={1}>
-            <Form.Item label={<span>Mother's Fullname&nbsp;
+                </Tooltip>
+              </span>}>
+                {getFieldDecorator('addprovince', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select your Address Province!'
+                    }
+                  ]
+                })(<Select onChange={value => this.setAddressProvince(value)}>
+                  {provinces.map(p => <Option key={p.value} value={p.value}>{p.name}</Option>)}
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col span={7} offset={1}>
+              <Form.Item label={<span>Address District&nbsp;
+              <Tooltip title="write based on Registration book">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>}>
+                {getFieldDecorator('adddistrict', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select your Address District!',
+                    }
+                  ],
+                })(<Select>
+                  {this.getDistrictByProvince(this.state.AddressProvince).map(d => <Option
+                    key={d.value}
+                    value={d.name}>
+                    {d.name}
+                  </Option>)}
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col span={7} offset={1}>
+              <Form.Item label={<span>Address Detail&nbsp;
+              <Tooltip title="write based on Registration book">
+                  <Icon type="question-circle-o" />
+                </Tooltip>
+              </span>}>
+                {getFieldDecorator('adddetail', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please input your Address Detail'
+                    }
+                  ],
+                })(<Input />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <Form.Item label={
+                <span>Father Name&nbsp;
+              <Tooltip title="If you have father, you must input this field">
+                    <Icon type="question-circle-o" />
+                  </Tooltip></span>
+              }>
+                {getFieldDecorator('fatherfullname')(<Input />)}
+              </Form.Item>
+            </Col>
+            <Col span={11} offset={1}>
+              <Form.Item label={<span>Mother's Fullname&nbsp;
               <Tooltip title="If you have mother, you must input this field">
-                <Icon type="question-circle-o" />
-              </Tooltip></span>}>
-              {getFieldDecorator('motherfullname')(<Input />)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={12}>
-            <Form.Item label="Receiving Organization">
-              {getFieldDecorator('recievingorganization', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select receiving organization!'
-                  },
-                ],
-              })(<Select onChange={value => this.setState({ ApointmentProvince: value })}>
-                {provinces.map(p => <Option key={p.value} value={p.name}>{p.name}</Option>)}
-              </Select>)}
-            </Form.Item>
-          </Col>
-          <Col span={11} offset={1}>
-            <Form.Item label="Receiving Address">
-              {getFieldDecorator('receivingaddress', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please select receiving organization!'
-                  }
-                ]
-              })(<Input />)}
-            </Form.Item>
-          </Col>
-        </Row>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit Form
+                  <Icon type="question-circle-o" />
+                </Tooltip></span>}>
+                {getFieldDecorator('motherfullname')(<Input />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <Form.Item label="Receiving Organization">
+                {getFieldDecorator('recievingorganization', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select receiving organization!'
+                    },
+                  ],
+                })(<Select onChange={value => this.setState({ ApointmentProvince: value })}>
+                  {provinces.map(p => <Option key={p.value} value={p.name}>{p.name}</Option>)}
+                </Select>)}
+              </Form.Item>
+            </Col>
+            <Col span={11} offset={1}>
+              <Form.Item label="Receiving Address">
+                {getFieldDecorator('receivingaddress', {
+                  rules: [
+                    {
+                      required: true,
+                      message: 'Please select receiving organization!'
+                    }
+                  ]
+                })(<Input />)}
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit Form
           </Button>
-        </Form.Item>
-      </Form>
+          </Form.Item>
+        </Form>
+        <Modal title="Dang ky thanh cong"
+          visible={this.state.modalVisibility}
+          onOk={() => this.setState({ modalVisibility: false })}
+          oncancel={() => this.setState({ modalVisibility: false })}>
+        </Modal>
+      </div>
     );
   }
 }
